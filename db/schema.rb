a42c5c5f1ba92b458f0161924bb8360702f1a022
Shane Nicholson <shanescottnicholson@gmail.com>
2022-08-11 14:36:15 -0700

    Adds equipment model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_213439) do
+ActiveRecord::Schema.define(version: 2022_08_11_213547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "equipment", force: :cascade do |t|
+    t.string "name"
+    t.string "contents"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sheets", force: :cascade do |t|
     t.integer "user_id"
